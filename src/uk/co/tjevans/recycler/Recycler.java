4b68b896665b84abb48c9c4cb1da7539bc1e05f7
Jesse McCullough <headgam3z@gmail.com>
2015-11-15 20:15:59 -0500

    Testing

---
+++ src/uk/co/tjevans/recycler/Recycler.java
@@ -0,0 +1,21 @@
+package uk.co.tjevans.recycler;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Recycler's main class.
+ *
+ */
+public class Recycler extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// to do
+	}
+	
+	@Override
+	public void onDisable() {
+		// to do
+	}
+
+}
