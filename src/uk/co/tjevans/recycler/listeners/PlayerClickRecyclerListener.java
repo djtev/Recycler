9805fa8cdab64484f5977844e3e9cd45e0b2c499
Jesse McCullough <headgam3z@gmail.com>
2015-11-16 22:43:15 -0500

    Added a todo listener for PlayerClickRecyclerEvent

---
+++ src/uk/co/tjevans/recycler/listeners/PlayerClickRecyclerListener.java
@@ -0,0 +1,10 @@
+package uk.co.tjevans.recycler.listeners;
+
+public class PlayerClickRecyclerListener {
+
+	// to do
+	// listen to the PlayerClickRecyclerEvent
+	// execute recycling actions
+	// profit - lol
+	
+}
