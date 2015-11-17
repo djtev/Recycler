eb189630b2b1abc70df57a569b95a73b4ba92676
Jesse McCullough <headgam3z@gmail.com>
2015-11-16 22:45:00 -0500

    Added initialization and event registration

--- src/uk/co/tjevans/recycler/Recycler.java
+++ src/uk/co/tjevans/recycler/Recycler.java
@@ -1,21 +1,36 @@
 package uk.co.tjevans.recycler;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import uk.co.tjevans.recycler.events.PlayerClickRecyclerCallEvent;
+
 /**
- * Recycler's main class.
+ * Recycler's main class. Sets up its config, registers its events, and more.
  *
  */
 public class Recycler extends JavaPlugin {
 	
+	private RecyclerConfig config;
+	
+	public Recycler() {
+		this.config = new RecyclerConfig(this);
+	}
+	
 	@Override
 	public void onEnable() {
-		// to do
+		config.addDefaultValues();
+		PluginManager manager = getServer().getPluginManager();
+		manager.registerEvents(new PlayerClickRecyclerCallEvent(this), this);
 	}
 	
 	@Override
 	public void onDisable() {
 		// to do
+	}
+	
+	public RecyclerConfig getRecyclerConfig() {
+		return config;
 	}
 
 }
