51b7dab65bbcf4ea67296721c06d7e7564fa1751
Jesse McCullough <headgam3z@gmail.com>
2015-11-19 13:54:27 -0500

    Small framework for listener

--- src/uk/co/tjevans/recycler/listeners/PlayerClickRecyclerListener.java
+++ src/uk/co/tjevans/recycler/listeners/PlayerClickRecyclerListener.java
@@ -1,10 +1,24 @@
 package uk.co.tjevans.recycler.listeners;
 
-public class PlayerClickRecyclerListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import uk.co.tjevans.recycler.customevents.PlayerClickRecyclerEvent;
+
+public class PlayerClickRecyclerListener implements Listener {
 
 	// to do
 	// listen to the PlayerClickRecyclerEvent
 	// execute recycling actions
 	// profit - lol
+	
+	@EventHandler
+	public void onPlayerClickRecycler(PlayerClickRecyclerEvent e) {
+		switch (e.getItemToRecycle().getType()) {
+		
+		default:
+			break;
+		}
+	}
 	
 }
