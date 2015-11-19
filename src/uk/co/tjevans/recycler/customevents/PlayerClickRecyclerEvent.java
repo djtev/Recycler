7dfddfe5d04ffd03b1ca933f3aba35d276060217
Jesse McCullough <headgam3z@gmail.com>
2015-11-19 13:08:43 -0500

    Added itemToRecycle to PlayerClickRecyclerEvent

--- src/uk/co/tjevans/recycler/customevents/PlayerClickRecyclerEvent.java
+++ src/uk/co/tjevans/recycler/customevents/PlayerClickRecyclerEvent.java
@@ -3,6 +3,7 @@ package uk.co.tjevans.recycler.customevents;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Recycler's custom event for when a player clicks on the Recycler.
@@ -12,15 +13,31 @@ public class PlayerClickRecyclerEvent extends Event {
 	
 	private static final HandlerList handlers = new HandlerList();
 	private Player playerWhoClicked;
+	private ItemStack itemToRecycle;
 	
-	public PlayerClickRecyclerEvent(Player playerWhoClicked) {
+	public PlayerClickRecyclerEvent(Player playerWhoClicked, ItemStack itemToRecycle) {
 		this.playerWhoClicked = playerWhoClicked;
+		this.itemToRecycle = itemToRecycle;
 	}
 	
+	/**
+	 * Gets the player who clicked on the Recycler block.
+	 * 
+	 * @return the player
+	 */
 	public Player getPlayer() {
 		return playerWhoClicked;
 	}
 
+	/**
+	 * Gets the item the player wants to recycle.
+	 * 
+	 * @return the item
+	 */
+	public ItemStack getItemToRecycle() {
+		return itemToRecycle;
+	}
+	
 	@Override
 	public HandlerList getHandlers() {
 		return handlers;
