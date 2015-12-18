6351be752bc3c6e3f18bcbbc38375e7644e8a2a3
tom evans <148384@student.boston.ac.uk>
2015-12-18 18:29:57 +0000

    enchant check and durability test
    added a line to check for enchantment, if enchanted item will not be
    recyclable. also added a durability check on the bow as a test to check
    that durability can be used to deny if item is too broken.

--- src/uk/co/tjevans/recycler/events/PlayerClickRecyclerCallEvent.java
+++ src/uk/co/tjevans/recycler/events/PlayerClickRecyclerCallEvent.java
@@ -41,6 +41,10 @@ public class PlayerClickRecyclerCallEvent implements Listener {
 			if (itemToRecycle == null || itemToRecycle.getType() == Material.AIR) {
 				return;
 			}
+			
+			if (e.getPlayer().getItemInHand().hasItemMeta() && e.getPlayer().getItemInHand().getItemMeta().hasEnchants()==true){
+				e.getPlayer().sendMessage("Item " + type + " has enchants. Sorry but it is not Recyclable");
+				}
 			else if (itemToRecycle.getType() == Material.HOPPER && itemToRecycle.getAmount() >= 1){
 				e.getPlayer().getInventory().removeItem(new ItemStack(Material.HOPPER, 1));
 				e.getPlayer().getInventory().addItem(new ItemStack(Material.IRON_INGOT, 5));
@@ -55,12 +59,16 @@ public class PlayerClickRecyclerCallEvent implements Listener {
 				e.getPlayer().sendMessage("Item " + type + " Recycled");
 			}
 			else if (itemToRecycle.getType() == Material.BOW && itemToRecycle.getAmount() >= 1){
+				if (e.getPlayer().getItemInHand().getDurability()<=300){
+					e.getPlayer().sendMessage("Item " + type + " Is too damaged. Sorry but it is not Recyclable");
+					}
+				else {
 				e.getPlayer().getInventory().removeItem(new ItemStack(Material.BOW, 1));
 				e.getPlayer().getInventory().addItem(new ItemStack(Material.STRING, 3));
 				e.getPlayer().getInventory().addItem(new ItemStack(Material.STICK, 3));
 				e.getPlayer().sendMessage("Item " + type + " Recycled");
-			}
-			else if ((itemToRecycle.getType() == Material.FLINT) || (itemToRecycle.getType() == Material.RECORD_3) || (itemToRecycle.getType() == Material.RECORD_10) || (itemToRecycle.getType() == Material.RECORD_11) || (itemToRecycle.getType() == Material.RECORD_12) || (itemToRecycle.getType() == Material.RECORD_4) || (itemToRecycle.getType() == Material.RECORD_5) || (itemToRecycle.getType() == Material.RECORD_6) || (itemToRecycle.getType() == Material.RECORD_7) || (itemToRecycle.getType() == Material.RECORD_8) || (itemToRecycle.getType() == Material.RECORD_9) || (itemToRecycle.getType() == Material.FEATHER) || (itemToRecycle.getType() == Material.RED_ROSE) || (itemToRecycle.getType() == Material.APPLE) || (itemToRecycle.getType() == Material.DIAMOND)){
+			}}
+			else if ((itemToRecycle.getType() == Material.FLINT) || (itemToRecycle.getType() == Material.RECORD_3) || (itemToRecycle.getType() == Material.RECORD_10) || (itemToRecycle.getType() == Material.RECORD_11) || (itemToRecycle.getType() == Material.RECORD_12) || (itemToRecycle.getType() == Material.RECORD_4) || (itemToRecycle.getType() == Material.RECORD_5) || (itemToRecycle.getType() == Material.RECORD_6) || (itemToRecycle.getType() == Material.RECORD_7) || (itemToRecycle.getType() == Material.RECORD_8) || (itemToRecycle.getType() == Material.RECORD_9) || (itemToRecycle.getType() == Material.FEATHER) || (itemToRecycle.getType() == Material.RED_ROSE) || (itemToRecycle.getType() == Material.APPLE) || (itemToRecycle.getType() == Material.DIAMOND) || (itemToRecycle.getType() == Material.GOLD_NUGGET)|| (itemToRecycle.getType() == Material.NAME_TAG)){
 				e.getPlayer().sendMessage(type + "is not recyclable");
 			}
 				else {
